8a17d258a893baed4fd5f13e8f10031e62c90ead
andreffs18 <andreffs18@gmail.com>
2025-08-25 22:48:27 +0100

    Fix oh-my-zsh.sh and speed-pasting-zsh.sh

--- config/apps/speed-pasting-zsh.sh
+++ config/apps/speed-pasting-zsh.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Download oh-my-zsh framework
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) -s --unattended"
-
-# Overwrite installed .zshrc from oh-my-zsh with my own
-ln -sfv $DIRECTORY/config/system/.zshrc ~
-
-# Download plugins (DON'T FORGET, in case you want to add more, to add them to list of "plugins" on ~/.dotfiles/config/system/.zshrc!)
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
-
-
 # This speeds up pasting w/ autosuggest
 # source: https://github.com/zsh-users/zsh-autosuggestions/issues/238
 pasteinit() {
@@ -24,5 +12,4 @@ pastefinish() {
 zstyle :bracketed-paste-magic paste-init pasteinit
 zstyle :bracketed-paste-magic paste-finish pastefinish
 
-# Print zsh version
-log.success "Installed oh-my-zsh on $(zsh --version)!"
+log.success "Speed up pasting on zsh $(zsh --version)!"
